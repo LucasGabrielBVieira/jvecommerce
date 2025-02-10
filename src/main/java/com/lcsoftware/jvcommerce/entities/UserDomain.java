b8eeeac9a30424b3fcfe89ac509302effbc71959
Lucas Gabriel Babolim Vieira <lucasvieira0420@gmail.com>
2025-02-10 20:58:09 -0300

    Mapeamento da entidade UserDomain

--- src/main/java/com/lcsoftware/jvcommerce/entities/UserDomain.java
+++ src/main/java/com/lcsoftware/jvcommerce/entities/UserDomain.java
@@ -1,24 +1,32 @@
 package com.lcsoftware.jvcommerce.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import java.time.LocalDate;
 
-@EntityScan
+@Entity
+@Table(name= "tb_user")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class UserDomain {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String email;
+
     private String phone;
+
     private LocalDate birthDate;
+
     private String password;
 }

e9ccf0145494f01ce3faa3d1518796b41706b199
Lucas Gabriel Babolim Vieira <lucasvieira0420@gmail.com>
2025-02-11 21:02:33 -0300

    PaymentDomain created

---
+++ src/main/java/com/lcsoftware/jvcommerce/entities/PaymentDomain.java
@@ -0,0 +1,30 @@
+package com.lcsoftware.jvcommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name="tb_payment")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private OrderDomain order;
+
+}
